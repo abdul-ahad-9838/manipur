cab93d0a8744146e3f21f0477d16dbf9848c6d01
Abdul Ahad <abdulahad@edtechinnovate.com>
2026-06-03 14:15:44 +0530

    Revert "export added in mjs"
    This reverts commit 8e6c107300b9bba5719475ec04dab1b9e467be05.

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "standalone",
+  output: 'standalone',
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com" },
-      { protocol: "https", hostname: "**.cloudinary.com" },
-      { protocol: "https", hostname: "**.googleapis.com" },
-      { protocol: "https", hostname: "**" },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: '**.cloudinary.com' },
+      { protocol: 'https', hostname: '**.googleapis.com' },
+      { protocol: 'https', hostname: '**' },
     ],
   },
-  output: "export",
 };
 
 export default nextConfig;
