73f160624559d7715db1a36bf16e5977383e44d8
Abdul Ahad <abdulahad@edtechinnovate.com>
2026-06-13 13:01:24 +0530

    fix image in banner

--- src/components/BannerSection.jsx
+++ src/components/BannerSection.jsx
@@ -2,20 +2,21 @@ import Image from "next/image";
 import "@/styles/BannerSection.css";
 
 const BannerSection = ({ data, url }) => {
+  console.log(data?.image);
+  console.log(data.coverImage);
   return (
     <div className="school-hero" style={{ "--school-color": data.color }}>
       <div className="school-hero-bg">
-        {data?.image ||
-          (data.coverImage && (
-            <Image
-              src={data?.image || data.coverImage}
-              alt={data?.name || "Banner"}
-              fill
-              priority
-              sizes="100vw"
-              className="school-hero-image"
-            />
-          ))}
+        {(data?.image || data?.coverImage) && (
+          <Image
+            src={data?.image || data?.coverImage}
+            alt={data?.name || "Banner"}
+            fill
+            priority
+            sizes="100vw"
+            className="school-hero-image"
+          />
+        )}
       </div>
 
       <div className="school-hero-overlay" />
