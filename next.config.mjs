3e428b8f0a2228e57130de2895ba776b455135f9
Abdul Ahad <abdulahad@edtechinnovate.com>
2026-07-08 17:51:58 +0530

    added config to access admin/blogs to access from www domain

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,31 @@ const nextConfig = {
       { protocol: "https", hostname: "upload.wikimedia.org" },
     ],
   },
+  async headers() {
+    return [
+      {
+        // Applies CORS rules to all endpoints matching /api/
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          // Replace * with your exact client domain to improve security
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.miu.edu.in/",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
